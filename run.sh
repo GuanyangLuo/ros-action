a000a25577fc5837e131bb7f79cb7eb019e0c448
Guanyang Luo <luoguanyang@gmail.com>
2020-08-14 11:50:06 -0700

    Try to add an artifact

--- run.sh
+++ run.sh
@@ -11,5 +11,8 @@ catkin_make
 
 source devel/setup.bash
 
+touch /github/home/test
+echo "hello" > /github/home/test
+
 timeout 3m roslaunch raceon_simulation raceon_simulation.launch || exit 0
 # python /race-on-ws/src/raceon_simulation/scripts/launch_environment.py 
